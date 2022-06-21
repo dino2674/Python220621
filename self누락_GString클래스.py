77c7baa10f44452e631ee26b4de7710c22a9ad79
dino2674 <yjsong9689@gmail.com>
2022-06-21 14:33:43 +0900

    클래스 연습 4

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,15 @@
+#전역 변수
 str = "Not Class Member"
+
+#클래스를 정의
 class GString:
     def __init__(self):
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        #약간의 버그(실수)
+        print(self.str)
 
 g = GString()
 g.set("First Message")
