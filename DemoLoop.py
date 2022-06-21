2ccac1608acf7c3b02459db0508fac6127f6c0fe
dino2674 <yjsong9689@gmail.com>
2022-06-21 10:38:44 +0900

    반복 구문 연습

---
+++ DemoLoop.py
@@ -0,0 +1,15 @@
+# DemoLoop.py
+
+#반복 구문
+value = 5
+while value > 0:
+    print(value)
+    value -= 1
+
+print ("--- 반복 종료 ---")
+
+#순화 가능한 형식 (시퀀스 형식)
+for item in [1,2,3]:
+    print(item)
+
+    
