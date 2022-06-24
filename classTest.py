2ddb4d0837958c7a2679faf3f113233a718b36fe
dino2674 <yjsong9689@gmail.com>
2022-06-24 11:44:43 +0900

    차트 그리기 연습 1

---
+++ classTest.py
@@ -0,0 +1,11 @@
+class TESTA:
+    def __init__(self, value1, value2, value3, value4) -> None:
+        self.Value1 = value1
+        self.Value2 = value2
+        self.Value3 = value3
+        self.Value4 = value4
+
+
+
+tempTestA = TESTA()["A", 10, "B", 20]
+print(tempTestA.Value1)
